6e965ca615e17ec3eb248d29c9b1a899f243739e
ycpzko <50710126+ycpzko@users.noreply.github.com>
2020-03-09 11:26:56 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,10 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
+# 编译 po2lmo (如果有po2lmo可跳过)
+pushd package/luci-app-openclash/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
 #添加自定义组件
 git clone https://github.com/sypopo/luci-theme-atmaterial.git package/lean/luci-theme-atmaterial
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
@@ -17,7 +20,7 @@ git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
 mkdir -p package/parted && cp -i package/luci-app-diskman/Parted.Makefile package/parted/Makefile
 mkdir -p package/lean/smartdns && wget -P package/lean/smartdns https://raw.githubusercontent.com/openwrt/packages/master/net/smartdns/Makefile
 git clone https://github.com/Apocalypsor/luci-app-smartdns.git package/lean/luci-app-smartdns
-
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash 
 #修复核心及添加温度显示
 sed -i 's|pcdata(boardinfo.system or "?")|luci.sys.exec("uname -m") or "?"|g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 sed -i 's/or "1"%>/or "1"%> ( <%=luci.sys.exec("expr `cat \/sys\/class\/thermal\/thermal_zone0\/temp` \/ 1000") or "?"%> \&#8451; ) /g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
